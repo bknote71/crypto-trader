66f8bfae8c85d58224c2dc344c3e0c78b3d68431
bknote71 <bknote71@gmail.com>
2024-08-30 12:12:39 +0900

    first commit(add scheduler project)

---
+++ scheduler/src/test/java/com/crypto_trader/scheduler/SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto_trader.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
