99e692a2d03c83d105550347457db42fdaa34a27
bknote71 <bknote71@gmail.com>
2024-08-30 21:05:09 +0900

    add api-server

---
+++ api-server/src/test/java/com/crypto_trader/api_server/ApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto_trader.api_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
