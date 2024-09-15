3b726b1fdc84e131b3adb9bcf0a6ffc3d547262c
Donghyun Kim <ilovekdh1208@ajou.ac.kr>
2024-09-15 16:36:38 +0900

    feat: 모든 캔들 몽고디비에서 가져오는 기능 추가

---
+++ crypto-module/api-server/src/main/java/com/crypto_trader/api_server/domain/entities/Candle.java
@@ -0,0 +1,34 @@
+package com.crypto_trader.api_server.domain.entities;
+
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Document(collection = "candle")
+public class Candle {
+
+    @Id
+    private String id;
+    private String market;
+    private double open;
+    private double close;
+    private double high;
+    private double low;
+    private LocalDateTime time;
+    private double volume;
+
+    public Candle() {}
+
+    public Candle(String market, double open, double close, double high, double low) {
+        this.market = market;
+        this.open = open;
+        this.close = close;
+        this.high = high;
+        this.low = low;
+        this.time = LocalDateTime.now().withSecond(0).withNano(0);
+    }
+
+}
--- crypto-module/api-server/src/main/java/com/crypto_trader/api_server/presentation/CandleController.java
+++ crypto-module/api-server/src/main/java/com/crypto_trader/api_server/presentation/CandleController.java
@@ -1,11 +1,37 @@
 package com.crypto_trader.api_server.presentation;
 
+import com.crypto_trader.api_server.application.dto.CandleResponseDto;
+import com.crypto_trader.api_server.domain.entities.Candle;
+import com.crypto_trader.api_server.infra.CandleMongoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class CandleController {
 
-//    @GetMapping("/all-candles")
-//    public
+
+    private final CandleMongoRepository candleMongoRepository;
+
+    public CandleController(CandleMongoRepository candleMongoRepository) {
+        this.candleMongoRepository = candleMongoRepository;
+    }
+
+    @GetMapping("/api/all-candles")
+    public List<CandleResponseDto> getAllCandles(@RequestParam String market) {
+        // MongoDB에서 해당 market에 대한 모든 candle 가져오기
+        List<Candle> candles = candleMongoRepository.findCandlesByMarket(market);
+
+        // Candle 엔티티를 CandleResponseDto로 변환
+        List<CandleResponseDto> response = candles.stream()
+                .map(CandleResponseDto::new)
+                .collect(Collectors.toList());
+
+        return response;
+    }
 }
